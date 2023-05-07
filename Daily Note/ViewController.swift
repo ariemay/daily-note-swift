b72b393f43aa672e6e90b0d8aacc9c9dfcfc75de
Arie May Wibowo <arie.wibowo@accenture.com>
2023-05-07 22:28:34 +0700

    Initial Commit

---
+++ Daily Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily Note
+//
+//  Created by Arie Wibowo on 07/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
